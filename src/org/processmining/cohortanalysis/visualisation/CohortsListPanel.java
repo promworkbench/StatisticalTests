2f1bcbb8a9bcfcbeb252eefaae75288a662a6df3
sleemans <s.leemans@bpm.rwth-aachen.de>
2021-09-04 22:08:01 +0000

    temp disable IvM
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StatisticalTests/Trunk@45368 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/cohortanalysis/visualisation/CohortsListPanel.java
+++ src/org/processmining/cohortanalysis/visualisation/CohortsListPanel.java
@@ -6,8 +6,8 @@ import javax.swing.table.AbstractTableModel;
 
 import org.processmining.cohortanalysis.cohort.Cohort;
 import org.processmining.cohortanalysis.cohort.Cohorts;
+import org.processmining.plugins.inductiveVisualMiner.dataanalysis.DataAnalysisTable;
 import org.processmining.plugins.inductiveVisualMiner.dataanalysis.DataAnalysisTableCellRenderer;
-import org.processmining.plugins.inductiveVisualMiner.dataanalysis.DataTable;
 import org.processmining.plugins.inductiveVisualMiner.dataanalysis.DisplayType;
 
 public class CohortsListPanel extends JTable {
@@ -20,9 +20,9 @@ public class CohortsListPanel extends JTable {
 	public CohortsListPanel() {
 		setOpaque(false);
 		setShowGrid(false);
-		setRowMargin(DataTable.rowMargin);
-		setRowHeight(DataTable.rowHeight);
-		getColumnModel().setColumnMargin(DataTable.columnMargin);
+		setRowMargin(DataAnalysisTable.rowMargin);
+		setRowHeight(DataAnalysisTable.rowHeight);
+		getColumnModel().setColumnMargin(DataAnalysisTable.columnMargin);
 		setDefaultRenderer(Object.class, new DataAnalysisTableCellRenderer());
 
 		model = new AbstractTableModel() {
